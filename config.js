7c2a22e4386edbbfa08d150276b0e25815a13028
abraham perez <abramhperez@gmail.com>
2021-07-09 22:05:10 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '400147391408554',
+        clientSecret: '21b66094b04f745e9dc97415b996396d'
+    }
 }
